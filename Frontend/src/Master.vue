3cfcdbad78de373730a03ccfe91a59a522e482ba
SSGDoc <larspeter.main@hotmail.com>
2021-10-08 15:36:44 +0200

    Components and views created

---
+++ Frontend/src/Master.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+
+  <navbar></navbar>
+  <footer></footer>
+
+</div>
+</template>
+
+<script>
+import Navbar from './components/containers/Navbar.vue'
+import Footer from './components/containers/Footer.vue'
+export default {
+    name: "Master",
+    component: {
+        Navbar,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
