da98566e765691fe51bb188a286693ace182bc34
MOBA94 <62548438+MOBA94@users.noreply.github.com>
2021-10-15 13:32:38 +0200

    footer

---
+++ Frontend/src/components/containers/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer>
+    <p class="left">{{footerText}}</p>
+	<p class="center">Kontakt</p>
+	<p class="contact">Telefon: 60680085 <br>
+	Mail: hcgs@gmail.com
+	</p>
+  </footer>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			footerText: 'Bondeknolderiet'
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+footer {
+	background-color: #141618;
+	color: white;
+	bottom: 0;
+	width: 100%;
+	height: 150px;
+}
+
+.left{
+	float: left;
+	padding: 20px;
+	font-family: "Bleeding_Cowboys";
+	font-size: 24px;
+}
+
+.center{
+	font-family: "Archivo";
+	font-size: 25px;
+	padding-top: 10px;
+}
+
+.contact {
+	font-family: "Archivo";
+}
+
+</style>
