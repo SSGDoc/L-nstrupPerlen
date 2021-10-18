19e4e3005cdb7d39ca9405f2efd7a28e300c6f18
SSGDoc <larspeter.main@hotmail.com>
2021-10-18 11:58:54 +0200

    Routerfunctions

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <master></master>
   </div>
 </template>
+<script>
+import Master from './Master.vue'
+
+export default {
+  name: "App",
+  components: {
+    Master
+  },
+}
+</script>
 
 <style>
 #app {
@@ -12,18 +21,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
